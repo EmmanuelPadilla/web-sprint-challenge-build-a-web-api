d5a7a673f2304ec37a7fea33560d101ebbf56ce3
Emmanuel Padilla <manolin923@gmail.com>
2020-10-30 10:36:01 -0700

    project get, post and delete are working, action get, post working

--- data/helpers/actionsRouter.js
+++ data/helpers/actionsRouter.js
@@ -9,6 +9,34 @@ router.get('/actions', (req, res) => {
     .catch(err => res.status(500).json(`${err}`))
 })
 
+router.get('/actions/:id', (req, res) => {
+    Actions.get(req.params.id)
+    .then((action)=>{ res.status(200).json(action)})
+    .catch(err => res.status(500).json(`${err}`))
+})
+
+router.post('/actions/:id', (req, res) => {
+    const {description, notes, completed} = req.body
+    const id = req.params.id
+    if(!description || !notes){
+        res.status(400).json({message: "missing description and/or notes"})
+    } else{
+        const newAction = { project_id: id, description, notes, completed}
+        Actions.insert(newAction)
+        .then ((data) => { res.status(201).json(data)})
+        .catch(err => {res.status(500).json({message:"could not post action"})})
+    }
+})
+
+
+
+
+router.delete('/actions/:id', (req, res) =>{
+    Action.remove(req.params.id)
+    .then(() => res.status(200).json({ message: `action ${req.params.id} was destroyed`}))
+    .catch(err => res.status(500).json({message: "could not delete"}, err))
+})
+
 
 
 
--- data/helpers/projectsRouter.js
+++ data/helpers/projectsRouter.js
@@ -1,12 +1,47 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const Projects = require('./projectModel')
+const Projects = require("./projectModel");
 
-router.get('/projects', (req, res) => {
-    Projects.get()
-    .then((actions)=>{ res.status(200).json(actions)})
-    .catch(err => res.status(500).json(`${err}`))
-})
+router.get("/projects", (req, res) => {
+  Projects.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => res.status(500).json(`${err}`));
+});
+
+router.get("/projects/:id", (req, res) => {
+  Projects.get(req.params.id)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((err) => res.status(500).json(`${err}`));
+});
+router.get("/projects/:id", (req, res) => {
+  projects
+    .get(req.params.id)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((err) => res.status(500).json(`${err}`));
+});
+
+router.post("/projects/", (req, res) => {
+  const { name, description, completed } = req.body;
+
+  if (!description || !name) {
+    res.status(400).json({ message: "missing description and/or notes" });
+  } else {
+    const newProject = { name, description, completed };
+    Projects.insert(newProject)
+      .then((data) => {
+        res.status(201).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: "could not post project" });
+      });
+  }
+});
 
 
 
@@ -17,4 +52,13 @@ router.get('/projects', (req, res) => {
 
 
 
-module.exports = router
+
+router.delete("/projects/:id", (req, res) => {
+  Projects.remove(req.params.id)
+    .then(() =>
+      res.status(200).json({ message: `Project ${req.params.id} was destroyed` })
+    )
+    .catch((err) => res.status(500).json({ message: "could not delete" }, err));
+});
+
+module.exports = router;
