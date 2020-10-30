f5d444d5dd32e3dea2281f81a5355347d65e514d
Emmanuel Padilla <manolin923@gmail.com>
2020-10-30 11:07:17 -0700

    put-ing works in projects

--- data/helpers/projectsRouter.js
+++ data/helpers/projectsRouter.js
@@ -17,14 +17,14 @@ router.get("/projects/:id", (req, res) => {
     })
     .catch((err) => res.status(500).json(`${err}`));
 });
-router.get("/projects/:id", (req, res) => {
-  projects
-    .get(req.params.id)
-    .then((action) => {
-      res.status(200).json(action);
-    })
-    .catch((err) => res.status(500).json(`${err}`));
-});
+// router.get("/projects/:id", (req, res) => {
+//   projects
+//     .get(req.params.id)
+//     .then((action) => {
+//       res.status(200).json(action);
+//     })
+//     .catch((err) => res.status(500).json(`${err}`));
+// });
 
 router.post("/projects/", (req, res) => {
   const { name, description, completed } = req.body;
@@ -43,7 +43,45 @@ router.post("/projects/", (req, res) => {
   }
 });
 
+router.put("/projects/:id", (req, res)=>{
+const changes = req.body
+Projects.update(req.params.id, changes)
+.then(project => {
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ message: 'The project could not be found' });
+    }
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the project',
+    });
+  });
+})
+//     const { id } = req.params
+//     const { name, description, completed } = req.body;
+//     const indexOfProject = projects.findIndex((project) => project.id ===id)
+//   if (indexOfProject !== -1) {
+//       project[indexOfProject] = { id, name, description, completed}
+//     res.status(200).json({ id, name, description, completed });
+//   } else {
+//       res.status(400).json({ message: `No Project with id ${id}`})
+//   }
+// })
+//     const editedProject = { name, description, completed };
+//     Projects.update(id, ...editedProject)
+//       .then((data) => {
+//         res.status(201).json(data);
+//       })
+//       .catch((err) => {
+//         res.status(500).json({ message: "could not update project" });
+//       });
+//   }
 
+// })
 
 
 
